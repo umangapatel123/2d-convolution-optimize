13c0a163f62968c7395c4b059516c68fc1805f75
Umang Patel <umangapatel123@gmail.com>
2024-04-07 02:41:49 +0530

    simd

--- src/main.cpp
+++ src/main.cpp
@@ -16,39 +16,48 @@ namespace solution{
 		const auto img = std::make_unique<float[]>(num_rows * num_cols);
 		bitmap_fs.read(reinterpret_cast<char*>(img.get()), sizeof(float) * num_rows * num_cols);
 		bitmap_fs.close();
-		// for(std::int32_t k = 0; k < num_rows * num_cols-9; k+=8){
-		//     float sum = 0.0;
-		//     int i = k / num_cols, j = k % num_cols;
-		// 	__m256 sum_v = _mm256_setzero_ps();
-		// 	for(int di = -1; di <= 1; di++)
-		//         for(int dj = -1; dj <= 1; dj++) {
-		//         	int ni = i + di, nj = j + dj;
-		//             if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_cols) {
-		//             	__m256 img_v = _mm256_loadu_ps(&img[ni * num_cols + nj]);
-		//             	__m256 kernel_v = _mm256_set1_ps(kernel[di + 1][dj + 1]);
-		//             	sum_v = _mm256_fmadd_ps(img_v, kernel_v, sum_v);
-		//             }
-	    //     	}
-	    //     _mm256_storeu_ps(&sum, sum_v);
-		// 	sol_fs.write(reinterpret_cast<char*>(&sum), sizeof(sum));
-		// }
-		for(std::int32_t i = 0; i < num_rows-1; i++){
-			for(std::int32_t j = 0; j < num_cols-8; j+=8){
-				float sum = 0.0;
-				__m256 sum_v = _mm256_setzero_ps();
-				for(std::int32_t di = -1; di <= 1; di++){
-					for(std::int32_t dj = -1; dj <= 1; dj++){
-						std::int32_t ni = i + di, nj = j + dj;
-						if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_cols){
-							__m256 img_v = _mm256_loadu_ps(&img[ni * num_cols + nj]);
-							__m256 kernel_v = _mm256_set1_ps(kernel[di + 1][dj + 1]);
-							sum_v = _mm256_fmadd_ps(img_v, kernel_v, sum_v);
+		for(std::int32_t k = 0; k < num_rows * num_cols; k++){
+				float sum[8] = {0.0};
+				int i = k / num_cols, j = k % num_cols;
+				if(i == 0 or i == num_rows - 1 or j == 0 or j == num_cols - 1){
+					float sum = 0.0;
+					for(std::int32_t di = -1; di <= 1; di++)
+						for(std::int32_t dj = -1; dj <= 1; dj++) {
+							std::int32_t ni = i + di, nj = j + dj;
+							if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_cols)
+								sum += kernel[di+1][dj+1] * img[ni * num_cols + nj];
+						}
+					sol_fs.write(reinterpret_cast<char*>(&sum), sizeof(sum));
+					continue;
+				}
+				if(j+7 < num_cols){
+					__m256 sum_v = _mm256_setzero_ps();
+					for(std::int32_t di = -1; di <= 1; di++){
+						for(std::int32_t dj = -1; dj <= 1; dj++){
+							std::int32_t ni = i + di, nj = j + dj;
+							if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_cols){
+								__m256 img_v = _mm256_loadu_ps(&img[ni * num_cols + nj]);
+								__m256 kernel_v = _mm256_set1_ps(kernel[di + 1][dj + 1]);
+								sum_v = _mm256_fmadd_ps(img_v, kernel_v, sum_v);
+							}
 						}
 					}
+					_mm256_storeu_ps(sum, sum_v);
+					for(std::int32_t i = 0; i < 8; i++){
+						sol_fs.write(reinterpret_cast<char*>(&sum[i]), sizeof(sum[i]));
+					}
+					k += 7;
 				}
-				_mm256_storeu_ps(&sum, sum_v);
-				sol_fs.write(reinterpret_cast<char*>(&sum), sizeof(sum));
-			}	
+				else{
+					float sum = 0.0;
+					for(std::int32_t di = -1; di <= 1; di++)
+						for(std::int32_t dj = -1; dj <= 1; dj++) {
+							std::int32_t ni = i + di, nj = j + dj;
+							if(ni >= 0 and ni < num_rows and nj >= 0 and nj < num_cols)
+								sum += kernel[di+1][dj+1] * img[ni * num_cols + nj];
+						}
+					sol_fs.write(reinterpret_cast<char*>(&sum), sizeof(sum));
+				}
 		}
 		sol_fs.close();
 		return sol_path;
